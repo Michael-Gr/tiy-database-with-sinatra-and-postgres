58f4a3e7f78d6bef66a83e8e52f7c416098b7042
Michael Grein <michaelgrein92@gmail.com>
2016-12-05 09:14:06 -0500

    Everything except search and edit from explorer mode

--- app.rb
+++ app.rb
@@ -13,6 +13,15 @@ get "/employees" do
   erb :employees
 end
 
+get "/show_employee" do
+  id = params["id"]
+
+  database = PG.connect(dbname: 'tiy-database')
+  @rows = database.exec('SELECT * FROM employees where id = $1', [id])
+
+  erb :show_employee
+end
+
 get "/new_employee" do
   erb :new_employee
 end
@@ -22,12 +31,58 @@ post "/create_employee" do
   address = params["address"]
   phone = params["phone"]
   salary = params["salary"]
-  position = params["position"]
   github = params["github"]
   slack = params["slack"]
+  position = params["position"]
 
   database = PG.connect(dbname: "tiy-database")
-  database.exec("INSERT INTO employees(name, address, phone, salary, position, github, slack) VALUES($1, $2, $3, $4, $5, $6, $7)",[name, address, phone, salary, position, github, slack])
+  database.exec("INSERT INTO employees(name, address, phone, salary, github, slack, position) VALUES($1, $2, $3, $4, $5, $6, $7)",[name, address, phone, salary, github, slack, position])
+
+  redirect to("/employees")
+end
+
+get "/edit_employee" do
+  id = params["id"]
+
+  database = PG.connect(dbname: 'tiy-database')
+  @rows = database.exec('SELECT * FROM employees where id = $1', [id])
+
+  erb :edit_employee
+end
+
+post "/submit_edit" do
+  id = params["id"]
+
+  database = PG.connect(dbname: "tiy-database")
+  database.exec('DELETE FROM employees WHERE employees.id = $1', [id])
+
+  name = params["name"]
+  address = params["address"]
+  phone = params["phone"]
+  salary = params["salary"]
+  github = params["github"]
+  slack = params["slack"]
+  position = params["position"]
+
+  database.exec("INSERT INTO employees(name, address, phone, salary, github, slack, position) VALUES($1, $2, $3, $4, $5, $6, $7)",[name, address, phone, salary, github, slack, position])
+
+  redirect to("/employees")
+end
+
+get "/delete_employee" do
+  id = params["id"]
+
+  database = PG.connect(dbname: 'tiy-database')
+  @rows = database.exec('SELECT * FROM employees where id = $1', [id])
+
+  erb :delete_employee
+end
+
+post "/delete" do
+  id = params["id"]
+
+  database = PG.connect(dbname: "tiy-database")
+  database.exec('DELETE FROM employees WHERE id = $1', [id])
 
   redirect to("/employees")
 end
